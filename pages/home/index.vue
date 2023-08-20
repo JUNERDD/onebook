7c356989a0b837c98fcf3f667c5dda2357aafa80
JUNER <53842108@qq.com>
2023-08-20 23:52:45 +0800

    ci(unocss): 完善unocss配置

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,22 +1,12 @@
 <script lang="ts" setup>
-import useHomeStore from '~/stores/home'
-
 definePageMeta({
   title: '首页',
   alias: '/'
 })
-const value1 = ref('')
-const handleClick = () => {
-  ElNotification.success('你好世界')
-}
-const homeStore = useHomeStore()
 </script>
 
 <template>
-  <div>
-    <el-button type="primary" @click="handleClick">{{ homeStore.name }}</el-button>
-    <el-date-picker v-model="value1" type="date" placeholder="Pick a day" />
-  </div>
+  <div>home</div>
 </template>
 
 <style scoped></style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,49 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig, presetAttributify, presetUno, presetWebFonts } from 'unocss'
 import theme from './assets/theme'
 
 export default defineConfig({
-  // ...UnoCSS options
-  theme
+  /* 主题 */
+  theme,
+  /* 预设 */
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetWebFonts({
+      provider: 'fontshare',
+      fonts: {
+        panchang: 'Panchang'
+      }
+    })
+  ],
+  /* 快捷方式 */
+  shortcuts: [
+    {
+      'full-box': 'w-full h-full'
+    },
+    [
+      /^flex-center(-\w)?/,
+      ([, w]) => {
+        switch (true) {
+          case !w:
+            return 'flex justify-center items-center'
+          case w === '-i':
+            return 'flex items-center'
+          case w === '-j':
+            return 'flex justify-center'
+        }
+      }
+    ],
+    [
+      // 主题色快捷方式
+      /^(\w+)-(\w+)-(\d+)$/,
+      ([, n, c, d]) => {
+        const num = Number(d)
+        // 如果数值为1直接返回原色
+        if (num === 1) return `${n}-${c}`
+        // 低于7返回带透明度的颜色
+        else if (num < 7) return `${n}-${c}:${105 - (num - 1) * 20}`
+      }
+    ]
+  ]
 })
