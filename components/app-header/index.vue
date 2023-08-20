48ef48895df8e8c5d231ea48b2e26dedaa41e09d
JUNER <53842108@qq.com>
2023-08-20 16:07:08 +0800

    refactor(index.d.ts): 修改声明文件

---
+++ components/app-header/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>Component: app-header/index</div>
+</template>
+
+<style scoped></style>
--- store/home/index.ts
+++
@@ -1,7 +0,0 @@
-const useHomeStore = defineStore('main', () => {
-  const count = ref(0)
-
-  return { count }
-})
-
-export default useHomeStore
