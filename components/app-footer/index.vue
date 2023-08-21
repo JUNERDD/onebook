51175640ed7352fe0244ebeee8ae9d35d6059408
JUNER <53842108@qq.com>
2023-08-21 23:50:15 +0800

    feat(app-header): 初始化底部搭建

--- components/app-footer/index.vue
+++ components/app-footer/index.vue
@@ -1,7 +1,16 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>Component: app-footer/index</div>
+  <div h30 mx2 flex-center b-t="1 dgrey solid">
+    <NuxtLink to="/">
+      <div i-ob-logo_text_white text="black dark:white-2" h10 w40></div>
+    </NuxtLink>
+    <!--
+    <NuxtLink to="https://beian.miit.gov.cn/" target="_blank">
+      <div pt5 text="black dark:white-2">网站备案/许可证号：粤ICP备2021099016号-1</div>
+    </NuxtLink>
+    -->
+  </div>
 </template>
 
 <style scoped></style>
