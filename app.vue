d9d5f341874e69860448f5bdd3494faa84f1c00e
JUNER <53842108@qq.com>
2023-08-22 23:07:16 +0800

    fix(dark,title): 修复部分bug
    1.修复系统深色模式首次打开显示浅色主题的bug。2.修复热加载路由中间件的useHead无效的bug

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+const route = useRoute()
+const title = computed(() => route.meta?.title)
 useHead({
-  // 标题模板（只有在vue文件中才生效）
-  titleTemplate: (title) => (title ? `${title} - One Book` : 'One Book')
+  // 标题模板（只有在vue文件中才生效 - 因为不可序列化，所以不能写在nuxt.config.ts中）
+  // titleTemplate: (title) => (title ? `${title} - One Book` : 'One Book')
+  titleTemplate: () => (title.value ? `${title.value} - One Book` : 'One Book')
 })
 </script>
 
--- public/head_script.ts
+++ public/head_script.ts
@@ -1,9 +1,12 @@
 // 在加载nuxt.js之间就进行主题切换
-const auto = localStorage.getItem('vueuse-color-scheme') === 'auto'
-const dark = localStorage.getItem('vueuse-color-scheme') === 'dark'
+const local = localStorage.getItem('vueuse-color-scheme')
+const auto = local === 'auto'
+const dark = local === 'dark'
 const windowDark = window.matchMedia('(prefers-color-scheme: dark)').matches
 
-if (auto && windowDark || dark) {
+if (!local && windowDark) {
+  document.documentElement.classList.add('dark')
+} else if (auto && windowDark || dark) {
   document.documentElement.classList.add('dark')
 } else {
   document.documentElement.classList.remove('dark')
