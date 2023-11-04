e820ac387d9c107f2b0852bfc7b00da1e7ba8992
JUNER <53842108@qq.com>
2023-11-05 00:10:03 +0800

    feat(home): 初始化响应式

--- components/custom/card/index.vue
+++ components/custom/card/index.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div border="1 solid" h-40 b-dgrey-5 dark:b-dgrey-6 rounded-2 p2>
+  <div
+    border="1 solid"
+    h-45
+    w="[calc(33.33333%-1.875rem*2/3)] lt-xl:[calc(50%-1.875rem*1/2)] lt-sm:full"
+    b-dgrey-5
+    dark:b-white-6
+    rounded-2
+    p2
+  >
     <h2>index</h2>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ definePageMeta({
   <div full-page-sub flex flex-col gap-9xl px-2>
     <!--    <div animate-zoom-in-down text="lt-sm:4xl sm:7xl" font="bold" select-none>Welcome.</div>-->
     <div mt-9xl flex flex-col gap-2 lt-sm="mt1">
-      <strong text="lt-sm:4xl sm:7xl">欢迎光临我的博客</strong>
+      <h1 text="lt-sm:5xl sm:7xl" leading="tight!">欢迎光临我的博客</h1>
       <p>
         嘿！你知道<custom-link
           link="https://music.163.com/#/artist?id=11074"
@@ -20,7 +20,7 @@ definePageMeta({
     </div>
 
     <div flex flex-col gap-2>
-      <h1>博客技术栈</h1>
+      <h1 text="lt-sm:2xl sm:4xl">博客技术栈</h1>
       <div flex flex-wrap gap-2>
         <custom-card />
         <custom-card />
