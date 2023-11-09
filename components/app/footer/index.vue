c10eb3f4e8b7e93186fdd75e7690b1e37ee1b0ae
JUNER <53842108@qq.com>
2023-11-09 12:49:18 +0800

    feat(app): 修改手机模式下的备案号字体大小

--- components/app/footer/index.vue
+++ components/app/footer/index.vue
@@ -6,7 +6,7 @@
       <div i-ob-logo_text_white h10 w40 auto-color></div>
     </NuxtLink>
     <NuxtLink to="https://beian.miit.gov.cn/" target="_blank">
-      <div auto-color>网站备案/许可证号：粤ICP备2021099016号-1</div>
+      <div lt-sm:text-xs auto-color>网站备案/许可证号：粤ICP备2021099016号-1</div>
     </NuxtLink>
   </div>
 </template>
