78b233c3e76ddbe129e3cb1094b28956f171af3c
JUNER <53842108@qq.com>
2023-11-09 12:48:24 +0800

    feat(app): 添加备案号

--- components/app/footer/index.vue
+++ components/app/footer/index.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div mx2 h30 flex-center b-t="1 dgrey solid">
+  <div mx2 flex-center-i justify-between py-2 b-t="1 dgrey solid" lt-sm="flex-col justify-center gap-2">
     <NuxtLink to="/">
       <div i-ob-logo_text_white h10 w40 auto-color></div>
+    </NuxtLink>
+    <NuxtLink to="https://beian.miit.gov.cn/" target="_blank">
+      <div auto-color>网站备案/许可证号：粤ICP备2021099016号-1</div>
     </NuxtLink>
   </div>
 </template>
