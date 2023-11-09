a1da9344eabd8bd6bb9b91c74a7f7e2e61be859d
JUNER <53842108@qq.com>
2023-11-09 10:55:37 +0800

    feat(home): 完善卡片内容

--- components/custom/card/index.vue
+++ components/custom/card/index.vue
@@ -8,7 +8,7 @@ defineProps<ICustomCardProps>()
   <div
     border="1 solid"
     w="[calc(33.33333%-1.25rem*2/3)] lt-xl:[calc(50%-1.25rem*1/2)] lt-sm:full"
-    h-45
+    h-40
     flex
     flex-col
     gap-4
@@ -18,7 +18,7 @@ defineProps<ICustomCardProps>()
     bg-dgrey-6
     p2
   >
-    <span text-1 :class="icon" />
+    <span :class="icon" shrink-0 text-1 />
     <h3>{{ label }}</h3>
     <div auto-color>{{ text }}</div>
   </div>
--- composables/home.ts
+++ composables/home.ts
@@ -3,43 +3,43 @@ export const useHomeStack = () => [
   {
     link: 'https://gitee.com/junerdd',
     label: 'Nuxt Architecture',
-    text: 'Harness the full power of Nuxt 3 and its modules ecosystem.',
+    text: '充分发挥 Nuxt 3 及其模块生态系统的全部潜力',
     icon: 'i-simple-icons-nuxtdotjs'
   },
   {
     link: 'https://gitee.com/junerdd',
     label: 'Vue Components',
-    text: 'Use built-in components (or your own!) inside your content.',
+    text: '组件化开发，运用 Vue 组件的强大功能',
     icon: 'i-simple-icons-vuedotjs'
   },
   {
     link: 'https://gitee.com/junerdd',
     label: 'TypeScript',
-    text: 'Complete typescript type check',
+    text: '完整的 TypeScript 类型检查',
     icon: 'i-simple-icons-typescript'
   },
   {
     link: 'https://gitee.com/junerdd',
     label: 'UnoCSS Engine',
-    text: 'Gitee',
+    text: '极致的原子化 CSS 引擎 UnoCSS',
     icon: 'i-simple-icons-unocss'
   },
   {
     link: 'https://gitee.com/junerdd',
     label: 'Drizzle ORM',
-    text: 'Gitee',
+    text: '以 Drizzle ORM 优化数据库交互',
     icon: 'i-ob-drizzle'
   },
   {
     link: 'https://gitee.com/junerdd',
     label: 'PlanetScale Database',
-    text: 'Gitee',
+    text: '使用 PlanetScale 数据库进行无缝扩展',
     icon: 'i-simple-icons-planetscale'
   },
   {
     link: 'https://gitee.com/junerdd',
     label: 'Vercel Deploy',
-    text: 'Gitee',
+    text: '利用 Vercel 进行快速部署和优化的前端体验',
     icon: 'i-simple-icons-vercel'
   }
 ]
