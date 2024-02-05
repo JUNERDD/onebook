44b4c877a858b97773cac8fbea8495db26ca9846
JUNER <53842108@qq.com>
2024-02-05 22:07:09 +0800

    revert(footer): 回退显示ba信息

--- components/app/footer/index.vue
+++ components/app/footer/index.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div mx2 flex-center py-2 b-t="1 bgrey dashed" lt-sm="flex-col justify-center gap-2">
+  <div mx2 flex-center-i justify-between py-2 b-t="1 dgrey solid" lt-sm="flex-col justify-center gap-2">
     <NuxtLink to="/">
       <div i-ob-logo_text_white h10 w40 auto-color></div>
     </NuxtLink>
-    <!--<NuxtLink to="https://beian.miit.gov.cn/" target="_blank">-->
-    <!--  <div lt-sm:text-xs auto-color>网站备案/许可证号：粤ICP备2021099016号-1</div>-->
-    <!--</NuxtLink>-->
+    <NuxtLink to="https://beian.miit.gov.cn/" target="_blank">
+      <div lt-sm:text-xs auto-color>网站备案/许可证号：粤ICP备2021099016号-1</div>
+    </NuxtLink>
   </div>
 </template>
 
